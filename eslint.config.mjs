6538f29b0ab3c098df679415d9d10cb545248b91
Achinta Sharma <23achinta@gmail.com>
2024-06-17 17:31:37 +0530

    add prettier, husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,36 @@
+// // eslint.config.js
+
+// import typescriptParser from "@typescript-eslint/parser";
+// import globals from "globals";
+
+// export default [
+//   {
+//     files: ["**/*.ts", "**/*.tsx"], // Include TypeScript files
+//     languageOptions: {
+//       parser: typescriptParser,
+//       globals: {
+//         ...globals.browser,
+//         ...globals.node,
+//       },
+//     },
+//     rules: {
+//       // semi: ["error", "always"],
+//       // "no-unused-vars": ["warn"],
+//       // "no-console": ["warn"],
+//       // indent: ["error", 2],
+//       // quotes: ["error", "single"],
+//     },
+//   },
+// ];
+
 // eslint.config.js
 
-import typescriptParser from "@typescript-eslint/parser";
-import globals from "globals";
+import typescriptParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   {
-    files: ["**/*.ts", "**/*.tsx"], // Include TypeScript files
+    files: ['**/*.ts', '**/*.tsx'], // Include TypeScript files
     languageOptions: {
       parser: typescriptParser,
       globals: {
@@ -13,12 +38,17 @@ export default [
         ...globals.node,
       },
     },
+    plugins: ['prettier'],
     rules: {
-      // semi: ["error", "always"],
-      // "no-unused-vars": ["warn"],
-      // "no-console": ["warn"],
-      // indent: ["error", 2],
-      // quotes: ["error", "single"],
+      'prettier/prettier': 'error',
+      semi: ['error', 'always'],
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'no-console': ['warn'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
     },
   },
 ];
